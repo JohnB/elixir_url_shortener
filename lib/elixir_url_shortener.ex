7a7f5475b1aafcf2278f163e21e511995621038b
JohnB <john.baylor@gmail.com>
2018-05-30 11:23:13 -0700

    Add random_short_code to correctly generate a random code.

--- lib/elixir_url_shortener.ex
+++ lib/elixir_url_shortener.ex
@@ -1,9 +1,25 @@
 defmodule ElixirUrlShortener do
-  @moduledoc """
-  ElixirUrlShortener keeps the contexts that define your domain
-  and business logic.
+  defstruct [:short_code, :long_url]
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  @short_code_length 8
+  @valid_short_characters [
+    "A", "B", "C", "D", "E",
+    "F", "G", "H", "J", "K",
+    "M", "N", "P", "Q", "R",
+    "S", "T", "W", "X", "Y",
+    "3", "4", "6", "7", "9"
+  ]
+
+  # The "internal" function seems like a cheat - but it was the only
+  # way to make it work for all lengths.
+  def random_short_code(size \\ @short_code_length) do
+    random_short_code_internal(size - 1) |> Enum.join
+  end
+
+  defp random_short_code_internal(size) when size < 0 do
+    []
+  end
+  defp random_short_code_internal(size) do
+    [Enum.random(@valid_short_characters) | random_short_code_internal(size - 1)]
+  end
 end
