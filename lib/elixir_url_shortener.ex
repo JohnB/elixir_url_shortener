68deaec6b04bd99206fad6c99fc132bafa51c63a
JohnB <john.baylor@gmail.com>
2018-05-30 12:57:24 -0700

    Ugly process to create singleton genserver to wrap short-to-long mappings.

--- lib/elixir_url_shortener.ex
+++ lib/elixir_url_shortener.ex
@@ -1,6 +1,44 @@
 defmodule ElixirUrlShortener do
+  @moduledoc """
+    Global singleton holding short-to-long mappings
+  """
+  
+  use GenServer
   defstruct [:short_code, :long_url]
+  
+  def start_link(_opts \\ []) do
+    case GenServer.start_link(__MODULE__, :ok, name: {:global, __MODULE__}) do
+      {:ok, pid} -> {:ok, pid}
+      {:error, {:already_started, pid}} -> {:ok, pid}
+    end
+  end
 
+  # Client side
+  def create_short_code(long_url) do
+    {:ok, our_pid} = start_link()
+    GenServer.call(our_pid, {:create_short_code, long_url})
+  end
+  def lookup_long_url(short_code) do
+    {:ok, our_pid} = start_link()
+    GenServer.call(our_pid, {:lookup_long_url, short_code})
+  end
+
+  # Server side
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:create_short_code, long_url}, _from, state) do
+    short_code = random_short_code()
+    shortener = %ElixirUrlShortener{short_code: short_code, long_url: long_url}
+    state = Map.put(state, short_code, shortener)
+    {:reply, short_code, state}
+  end
+
+  def handle_call({:lookup_long_url, short_code}, _from, state) do
+    {:reply, Map.fetch(state, short_code), state}
+  end
+  
   @short_code_length 8
   @valid_short_characters [
     "A", "B", "C", "D", "E",
