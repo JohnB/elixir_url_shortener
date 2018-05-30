ff69bf9df1ccbe399865d887b6e28ae47133f941
JohnB <john.baylor@gmail.com>
2018-05-30 14:16:59 -0700

    Add gigalixir support (hopefully).

--- config/prod.exs
+++ config/prod.exs
@@ -61,4 +61,21 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
+# RECOMMENDED BY GIGALIXIR
+config :elixir_url_shortener, ElixirUrlShortenerWeb.Endpoint,
+  load_from_system_env: true,
+  # http: [port: {:system, "PORT"}], # Uncomment this line if you are running Phoenix 1.2
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "example.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :elixir_url_shortener, ElixirUrlShortener.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 1 # Free tier db only allows 1 connection
+
+
